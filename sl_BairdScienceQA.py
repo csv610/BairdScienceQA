8bc702ac3d818b7386b13212f5e83ae78efae461
csv610 <csv610@yahoo.com>
2024-10-03 23:47:14 +0530

    updated questions

--- sl_BairdScienceQA.py
+++ sl_BairdScienceQA.py
@@ -2,6 +2,7 @@ import streamlit as st
 from llama_model import LlamaModel
 import json
 import random
+import time
 
 @st.cache_data
 def load_questions():
@@ -25,7 +26,7 @@ def get_llm_model(model_name):
 questions = load_questions()
 
 # Sidebar menu for selecting the subject
-st.sidebar.title("Select Subject")
+st.sidebar.title("Science Questions")
 subject = st.sidebar.selectbox("Choose a subject", list(load_questions().keys()))
 questions = questions[subject]
 
@@ -35,13 +36,18 @@ model_name = st.sidebar.selectbox('Select a model', model_names)
 llm = get_llm_model(model_name)
 
 # Add a button to generate a new random question
-if st.button("New Question"):
+if st.sidebar.button("New Question"):
     question = random.choice(questions)
     st.write(f"**Question:** {question}")
 
-    with st.spinner('Getting answer ...'):
-        answer = llm.get_response(question)
+    with st.spinner('Getting answer...'):
+         start_time = time.time()  # Record the start time
+         answer = llm.get_response(question)  # Get the response from the model
+         elapsed_time = time.time() - start_time  # Calculate the elapsed time
 
+    word_count = len(answer.split())
     st.write(f"**Answer:** {answer}")
+    st.write(f"**Word Count:** {word_count} words")
+    st.write(f"**Elapsed Time:** {elapsed_time:.2f} seconds")
 
 
